022aad68759d8f43ebbc80bc723ad4c24b144644
mbender <mbender@bentech.net>
2021-04-26 11:35:54 -0500

    Final Updates before Review of Lab 1

--- LAB_FILES/1_powershell-basics.ps1
+++ LAB_FILES/1_powershell-basics.ps1
@@ -1,11 +1,7 @@
 # Module - PowerShell Basics
-# URL: 
-
-# In this challenge, you will learn how to use various commands in PowerShell to explore the console, and find the information you are looking for. PowerShell's command line interface is built to provide you the information you need through exploration and use of the commands in the console.
-# Using this exploratory approach will deepen your understanding of how PowerShell works and allow you to find answers without always searching on the web for them.
 
 ##### Notes and helpful Items - PLEASE READ #####
-# USE ALT+Z to toggle Word Wrap for better text viewing in VS Code!!!
+# USE ALT+Z to toggle Word Wrap on the remarks for easier reading in VS Code.
 
 # Unless specified, Windows PowerShell and PowerShell 7 consoles need to be launced as Administrator using 'run as administrator' option by right-clicking icon.
 
@@ -26,192 +22,187 @@
 #IMPORTANT: Prior to beginning this lab, make sure you have completed the setup tasks in ./0_Start-Here-setup-lab.ps1!!
 #####
 
-# Demo: 
-# Run these commands in Windows PowerShell
-#In this challenge, you will walk through the demo from the beginning of the course. The demo walks you through creating a .csv report of all the stopped services on a remote server. While you may not know what the commands are doing yet, this will get you hands on with PowerShell, and get you practice typing commands into the console.
+# Pre-Lab Setup Steps - Complete before starting labs!!!
+# Add Client01 to Domain of company.co
+    # Steps
+    # 1) In the Lab_Files folder, right-click run-domainjoin.ps1 and choose 'Run as administrator'.
+    # 2) When prompted, enter username of Administrator and password from the companypw.txt file in LAB_Files directory.
+    # 3) After completing, Client01 will reboot and you may continue with the lab challenges.
 
 # Introduction to PowerShell
 # Demo: Using PowerShell to Report on Stopped Server Services
+    # Run these commands in Windows PowerShell console as Administrator
+    # Open Windows PowerShell
+    # Right-click on choose 'Run as administrator' on desktop icon
 
-# Run these commands in Windows PowerShell console
-# Open Windows PowerShell
-# Right-click on choose 'Run as administrator' on desktop icon
+    # View all services on local system                                                                                                                         
+        Get-Service
 
+    # View stopped services
+        get-service | where-Object Status -eq 'Stopped'
 
-# View all services on local system                                                                                                                         
-Get-Service
+    # View only Name and Status for stopped services                                                  
+        get-service | Where-Object Status -eq 'Stopped' | select-object Name,Status 
 
-# View stopped services
-get-service | where-Object Status -eq 'Stopped'
+    # Place command output in a variable
+        $data = get-service | Where-Object Status -eq 'Stopped' | select-object Name,Status 
+        $data
 
-# View only Name and Status for stopped services                                                  
-get-service | Where-Object Status -eq 'Stopped' | select-object Name,Status 
+    # Note the difference between the output files of the two commands below
+    # Output variable data to a .csv file
+    # View in notepad or VS Code
+        $data | out-file .\services.csv                  
+        notepad .\services.csv                                                                                               
+        code .\services.csv
 
-# Place command output in a variable
-$data = get-service | Where-Object Status -eq 'Stopped' | select-object Name,Status 
-$data
-
-# Note the difference between the output files of the two commands below
-# Output variable data to a .csv file
-$data | out-file .\services.csv                  
-notepad .\services.csv                                                                                               
-
-# Export variable data to a .csv file
-$data | export-csv .\Services2.csv
-                    
-get-content .\services2.csv | more  
+    # Export variable data to a .csv file
+        $data | export-csv .\Services2.csv   
+        get-content .\services2.csv | more  
 
 # Demo: Exploring PowerShell Verbs
-#In this first challenge, you will explore get-verb from the PowerShell console. PowerShell uses a Verb-Noun syntax so it's important that you learn the common verbs in PowerShell like get and set. 
-#Remember in PowerShell: Almost everything you need to know, you can find out through the shell. Just type and Explore.
-
+    # Run these commands in PowerShell 7 console
     #In the console window, type the following command to list all of the verbs available in the Windows PowerShell console, and click the space bar to scroll through each screen
-    get-verb | more
-        #The command includes | more so all of the output can be reviewed one screen at a time. If you need to exit the output, you can use Ctrl+C to go to the prompt.
+        get-verb | more
+            #The command includes | more so all of the output can be reviewed one screen at a time. If you need to exit the output, you can use Ctrl+C to go to the prompt.
 
     #Type the following command to view all of the commands in PowerShell using the Set verb.
-    get-verb -Verb Set | more
-        #In the command output, you will see the default formatting of output is as a table.
+        get-verb -Verb Set | more
+            #In the command output, you will see the default formatting of output is as a table.
 
     #Type the following command to view all of the commands in PowerShell using the set verb and in a list format.
-    get-verb -Verb Set | format-list
-        #In this command, you pipe "|" the output of the command into format-list to display the output as a list. 
+        get-verb -Verb Set | format-list
+            #In this command, you pipe "|" the output of the command into format-list to display the output as a list. 
 
     #Type the following command to view all of the verbs that are part of the Security group.
-    Get-Verb -Group Security | Format-List
-    #Note: The -group parameter is only available in PowerShell 7 so it will not work if you are using Windows PowerShell.
+        Get-Verb -Group Security | Format-List
+            #The -group parameter is only available in PowerShell 7 so it will not work if you are using Windows PowerShell.
 
     #Type Exit to close the PowerShell 7 console, or leave it open if you plan to do the next challenge.
-    Exit
-
-    # Broken
-    start https://aka.ms/psverbs
+        Exit
 
     # View Approved Verbs for Powershell doc at docs.microsoft.com.
     # Try https://bit.ly/psverbs
+    # This will not work in lab environment due to Internet access restrictions.
+    # Launch URL above in a separate browser window
 
 #Demo: Working with Aliases and Parameters
-# In this lab, you'll get experience using aliases and positional parameters in PowerShell. 
+    # Run these commands in Windows PowerShell console as Administrator
 
     # This command demonstrates gathering information with get-service
-    get-service -Name M* -ComputerName Client01,DC01
+        get-service -Name M* -ComputerName Client01,DC01
 
     # This command displays all aliases avaialable in Powershell console
-    Get-Alias | More
+        Get-Alias | More
 
     # This command displays
-    get-alias -Definition *service*
+        get-alias -Definition *service*
 
     # These commands shows the gsv alias in action
-    gsv -Name M* -ComputerName Client1,DC01
-    help gsv
+        gsv -Name M* -ComputerName Client1,DC01
+        help gsv
 
     # These commands show the use of aliases with positional parameters
-    gsv M* -Computername Client01,DC01
-    gsv M* -Comp Client1,Dc01
+        gsv M* -Computername Client01,DC01
+        gsv M* -Comp Client1,Dc01
 
 
 ## Demo: Finding Answers in the PowerShell Console
-#Demo 2 - Working with Help
     # Run as a normal user with PowerShell 7
     
     # Using Get-Help without help updated
-    get-help                           
-    help                  
-    get-help *Service* | more                        
-    help get-service            
+        get-help                           
+        help                  
+        get-help *Service* | more                        
+        help get-service            
 
     # Open PowerShell 7 and Run as Administrator (right-click icon)
     # Updating PowerShell 7
-    
-    update-help -SourcePath "c:\users\administrator\desktop\Lab_Files\pshelp"
+        update-help -SourcePath "c:\users\administrator\desktop\Lab_Files\pshelp"
+            #NOTE: You may receive errors for some updates failing. This is normal and can be ignored.
 
     # Using Help w/ different parameters 
-    help get-service
-    help get-service -Examples
-    help get-service -full
-    help get-service -online 
+        help get-service
+        help get-service -Examples
+        help get-service -full
+        help get-service -online 
 
-    Man get-service 
+        Man get-service 
 
     # Viewing about pages
-    Help *about*
-    Help about_Aliases
+        Help *about*
+        Help about_Aliases
 
 ## Demo: Researching Commands with Get-Command
-    Help get-command 
-    Help Get-Command -examples
-    get-command | more
-    Ctrl + C
-    get-command -verb New
-    get-command -CommandType Function | measure-object
-    
-    #You are looking for a command to work with IP Address Configurations on a Windows System
-    get-command -name *IP* | More
-    get-command -Name *IP* -Module Net*
-    Get-command -Name *IP* -Module NetTCPIP
-    Help Get-NetIPAddress 
-    Help Get-NetIPAddress -Examples
-    Get-NetIPAddress
+    # Run these commands in Windows PowerShell console as Administrator
 
+    # Exploring get-command
+        Help get-command 
+        Help Get-Command -examples
+        get-command | more
+        # Enter Ctrl + C to break out of get-command results
+    
+    # View all commands with the Verb New
+        get-command -verb New
+        get-command -CommandType Function | measure-object
+    
+    # Use get-command to find cmdlet for IP Address Configurations on a Windows System
+        get-command -name *IP* | More
+        get-command -Name *IP* -Module Net*
+        Get-command -Name *IP* -Module NetTCPIP
+        Help Get-NetIPAddress 
+        Help Get-NetIPAddress -Examples
+        Get-NetIPAddress
 
 ## Demo: Documenting Your Work in the PowerShell Console
-#Demo 4 - Using History and Transcript
+    # Run these commands in Windows PowerShell console as Administrator
 
-    # setup commands
-    Md c:\scripts\transcripts
-    cd c:\scripts
-    #cd c:\scripts
+    # setup commands to create and change directory
+        Md c:\scripts\transcripts
 
     #working with get-history
-    Help get-history
-    Get-history
+        Help get-history
+        Get-history
 
-    #For this command choose an id number from your history 
-    invoke-history -id 24
+    # Use invoke-command to access command from history by ID
+    # Note: your history will differ from video demo so choose an id number from your history 
+        invoke-history -id 24
 
     # Sending get-history output to a text file and viewing in notepad and vscode
-    Get-History | Out-File c:\scripts\transcripts\history.txt
-    notepad c:\scripts\transcripts\history.txt
-    code .\transcripts\history.txt
-    #Code c:\scripts\transcripts\history.txt
-
-    Clear-History
-    Get-History
+        Get-History | Out-File c:\scripts\transcripts\history.txt
+        notepad c:\scripts\transcripts\history.txt
+        code c:\scripts\transcripts\history.txt
+    
+    # Additional -history commands
+        Clear-History
+        Get-History
 
     # Working with start-transcript
-    Help Start-Transcript
-    Start-Transcript -path .\transcripts\transcript1.txt -Append
-    Get-service | Where-Object -Property Status -EQ Stopped
-    Stop-transcript
-    code .\Transcripts\transcript1.txt
-    Notepad .\Transcripts\transcript1.txt
+        Help Start-Transcript
+        Start-Transcript -path c:\scripts\transcripts\transcript1.txt -Append
+        Get-service | Where-Object -Property Status -EQ Stopped
+        Stop-transcript
+        
+    # View transcripts in notepad and vs code
+        code c:\scripts\transcripts\transcript1.txt
+        Notepad c:\scripts\transcripts\transcript1.txt
 
 # Demo: Finding Object Properties with Get-Member
-# Open Windows PowerShell from the Start Menu using Run As Administrator
+    # Run these commands in Windows PowerShell console as Administrator
 
-    Help Get-Member
-    Get-service | Get-Member
-    #Add -computername to commands ?
-    Get-service | Select-Object Name,MachineName,Status
-    Get-Service | Select-Object Name,MachineName,Status | Get-Member
-    Get-Service | Where-Object status -eq "Stopped" | More
+    # Explore Get-member cmdlet
+        Help Get-Member
+        Get-service | Get-Member
 
-    Get-Service -ComputerName Client01,DC01 |
-    >> Where-Object status -eq "Stopped" |
-    >> Select-Object Name,MachineName,Status |
-    >> Sort-Object -Property MachineName | More
+    # Viewing objects for get-service
+    # Add -computername to commands ?
+        Get-service | Select-Object Name,MachineName,Status
+        Get-Service | Select-Object Name,MachineName,Status | Get-Member
+        Get-Service | Where-Object status -eq "Stopped" | More
 
-
-# Not sure where this is from - Not in Module 3
-get-command -Name *Fire*
-get-command -Name get-*Fire*
-get-command -Name get-NetFire*
-help Get-NetFirewallRule
-Get-NetFirewallRule | gm
-Get-NetFirewallRule
-Get-NetFirewallRule -Name *Remote* 
-Get-NetFirewallRule -Name *RemoteDesktop* 
-Get-NetFirewallRule -Name *RemoteDesktop* | FT
-Get-NetFirewallRule -Name *RemoteDesktop* | Set-NetFirewallRule -Enabled 'True' -Whatif
-Get-NetFirewallRule -Name *RemoteDesktop* | FT
+    # Putting all the commands together
+    # Note: The video demo runs each command below line by line.
+    # To issue commands, type each line and issue 'Enter' after each pipe symbol.
+        Get-Service -ComputerName Client01,DC01 |
+        Where-Object status -eq "Stopped" |
+        Select-Object Name,MachineName,Status |
+        Sort-Object -Property MachineName | More
--- LAB_FILES/2_gathering-information.ps1
+++ LAB_FILES/2_gathering-information.ps1
@@ -1,117 +1,155 @@
-# Module - PowerShell Basics
-# URL: 
+# Module - Gathering Information with PowerShell
 
-# USE ALT+Z to toggle Word Wrap for better text viewing in VS Code!!!
 
-# M3-01
+##### Notes and helpful Items - PLEASE READ #####
+# USE ALT+Z to toggle Word Wrap on the remarks for easier reading in VS Code.
+
+# Unless specified, Windows PowerShell and PowerShell 7 consoles need to be launced as Administrator using 'run as administrator' option by right-clicking icon.
+
+# The lab environment has a single client (Client01) and a domain controller (DC01).
+
+# The lab environment has no internet access so actions requiring the Internet will not be performed.
+
+# All remote actions performed with Client02 as a target the video course demos will be performed with DC01 instead.
+
+# The commands below are not intended to be run in VS Code. They are intended to be type (preferred) or copy/pasted into the PowerShell console used in each demo.
+
+# Read all of the remarks (hash-tagged green lines) as non-command steps and information is included throughout this script.
+
+# Some demo commands have been modified to work within the lab environment. They will perform the same general tasks, but may have different references such as file directories. Also, any tasks requiring internet access will be skipped.
+
+# Use of cls in code is to clear-host or clear the screen during demos. This can be skipped during your labs if desired.
+
 # Demo: Finding Your Way in PowerShell
-# Run in Windows PowerShell console
-    get-command -Name *Fire*
-    get-command -Name get-*Fire*
-    get-command -Name get-NetFire*
-    help Get-NetFirewallRule
-    Get-NetFirewallRule | gm
-    Get-NetFirewallRule
-    Get-NetFirewallRule -Name *Remote* 
-    Get-NetFirewallRule -Name *RemoteDesktop* 
-    Get-NetFirewallRule -Name *RemoteDesktop* | FT
-    Get-NetFirewallRule -Name *RemoteDesktop* | Set-NetFirewallRule -Enabled 'True' -Whatif
-    Get-NetFirewallRule -Name *RemoteDesktop* | FT
+    # Run these commands in Windows PowerShell console as Administrator
+    
+    # Using get-command to find cmdlet for Windows Firewall
+        get-command -Name *Fire*
+        get-command -Name get-*Fire*
+        get-command -Name get-NetFire*
+        help Get-NetFirewallRule
+    
+    # Using get-member (alias gm) expose objects
+        Get-NetFirewallRule | gm
+        Get-NetFirewallRule
+    
+    # Retrieve Firewall rules for Remote Desktop
+        Get-NetFirewallRule -Name *Remote* 
+        Get-NetFirewallRule -Name *RemoteDesktop* 
+        Get-NetFirewallRule -Name *RemoteDesktop* | FT
+    
+    # Use pipeline to set Firewall rule
+        Get-NetFirewallRule -Name *RemoteDesktop* | Set-NetFirewallRule -Enabled 'True' -Whatif
+        Get-NetFirewallRule -Name *RemoteDesktop* | FT
 
-# M3-02
 # Demo: Gathering Computer Information
-# Run in Windows PowerShell console
-    Get-command *counter*
-    Help Get-Counter                                               
-    Get-Counter                                                                                                                                                                              
-    get-counter -ListSet *memory* 
-    get-counter -ListSet Memory 
+    # Run these commands in Windows PowerShell console as Administrator
+    
+    # Gathering counter information
+        Get-command *counter*
+        Help Get-Counter                                               
+        Get-Counter                                                                                                                                                                              
+        get-counter -ListSet *memory* 
+        get-counter -ListSet Memory 
     
     #View Path by Expanding
-    get-counter -ListSet Memory | Select -expand Counter
+        get-counter -ListSet Memory | Select -expand Counter
 
-    #error by design
-    get-counter -Counter "\Memory\Pages /sec","\Memory\% Committed Bytes In Use" | FT
-    get-counter -Counter "\Memory\Pages/Sec","\Memory\% Committed Bytes In Use" | FT
+    #  error by design
+    #  First command fails because counter name is incorrec for Memory Pages per Second
+        #Note the space before /sec
+        get-counter -Counter "\Memory\Pages /sec","\Memory\% Committed Bytes In Use" | FT
+        get-counter -Counter "\Memory\Pages/Sec","\Memory\% Committed Bytes In Use" | FT
 
-# M3-03
+
 # Demo: Using WMI and CIM Information
-# Run in Windows PowerShell
-    Get-WmiObject -List *
-    Get-CimClass -ClassName *
-    Get-CimClass -ClassName *memory*
-    Get-WmiObject -class Win32_PhysicalMemory
-    Get-CimInstance -ClassName Win32_PhysicalMemory
-    Get-CimInstance -ClassName Win32_PhysicalMemory | Select Tag,Capacity
+    # Run these commands in Windows PowerShell console as Administrator
+    
+    # Using WMI and CIM to find physical memory information
+    # Remember: -class and -classname parameter values are the same for most WMI/CIM calls
+        Get-WmiObject -List *
+        Get-CimClass -ClassName *
+        Get-CimClass -ClassName *memory*
+        Get-WmiObject -class Win32_PhysicalMemory
+        Get-CimInstance -ClassName Win32_PhysicalMemory
+        Get-CimInstance -ClassName Win32_PhysicalMemory | Select Tag,Capacity
 
-# M3-04
-# Demo: Working with Network Information 
-    ipconfig                                                                                 
-    ipconfig /all
-    ipconfig | gm                                                                          
-    Get-Command get-NetIP*                                                                                
-    Get-NetIPAddress                                                                         
-    Get-NetIPConfiguration
 
-    GCM get-*DNS* 
-    GCM get-DNSClient*                                                                  
-    Get-DnsClient                                                                                                                                              
-    Get-DnsClientCache                                                                                                                                              
-    Get-DnsClientServerAddress                                                               
+# Demo: Working with Network Information
+    # Run these commands in Windows PowerShell console as Administrator
 
-#Map a Network Drive > Use SMB or simple message block so we need to search for SMB related commands
-  
-    Get-Command *SMB*
-    Get-Command *SmbMapping                                                                        
-    Help New-SmbMapping -examples
-    New-SMBmapping -localPath w: -remotepath \\DC01\Share
-    Get-smbmapping
-    cd w:\
-    dir
-    #In the lab environment, the directory listing will have a different set of files and directories than the video demo
+    # Running ipconfig in Windows PowerShell
+        ipconfig                                                                                 
+        ipconfig /all
+        ipconfig | gm
+            #Note: No properties are displayed since command.exe executables linke ipconfig do not pass objects                                                                       
+    
+    # Accessing IP Address information
+        Get-Command get-NetIP*                                                                                
+        Get-NetIPAddress                                                                         
+        Get-NetIPConfiguration
+
+    # Accessing DNS Client Information
+        GCM get-*DNS* 
+        GCM get-DNSClient*                                                                  
+        Get-DnsClient                                                                                                                                              
+        Get-DnsClientCache                                                                                                                                              
+        Get-DnsClientServerAddress                                                               
+
+    #Map a Network Drive using Server Message Block (SMB)
+        Get-Command *SMB*
+        Get-Command *SmbMapping                                                                        
+        Help New-SmbMapping -examples
+        New-SMBmapping -localPath w: -remotepath \\DC01\Share
+        Get-smbmapping
+        cd w:\
+        dir
+        #Note: In the lab environment, the directory listing will have a different set of files and directories than the video demo
 
 # Demo: Reviewing Event Log Information
-# Run in Windows PowerShell console
-#Last, I want to see the last time the system had a reboot. The easiest way to see this is by finding the 1074 System event in Event Viewer. This event message signifies that the system has restarted. 
-    get-command get-*Event*
-    help get-eventlog -Examples
-    get-eventlog -LogName System | gm
 
-    # Enter each command on a single line
-    # last two commands will be enter at the '>>' prompt
-    Get-EventLog -log system -newest 1000 |
-    >>where-object {$_.eventid -eq '1074'} |
-    >>format-table machinename, username, timegenerated -autosize
+    # Run these commands in Windows PowerShell console as Administrator
 
+    # Retrieve Last Boot time from Event Logs using event 1074
+        get-command get-*Event*
+        help get-eventlog -Examples
+        get-eventlog -LogName System | gm
 
-#M3-04
+    # Note: The video demo runs each command below line by line.
+    # To issue commands, type each line and issue 'Enter' after each pipe symbol.
+        Get-EventLog -log system -newest 1000 |
+        where-object {$_.eventid -eq '1074'} |
+        format-table machinename, username, timegenerated -autosize
+
 # Demo: Using Get-ComputerInfo
-# Run in WindowsPowerShell
-    help Get-ComputerInfo
-    help Get-ComputerInfo -Examples
-    Get-ComputerInfo | more
-    Get-ComputerInfo -Property *memory*
 
+    # Run these commands in Windows PowerShell console as Administrator
 
-#m3-06-lab
+    # Retrieve memory information with Get-ComputerInfo
+        help Get-ComputerInfo
+        help Get-ComputerInfo -Examples
+        Get-ComputerInfo | more
+        Get-ComputerInfo -Property *memory*
+
 # Demo: Working with Files and Directories
-# Run in Windows PowerShell
-# Note: Files and directories in lab environment will be different than the video
+    # Run these commands in Windows PowerShell console as Administrator
+    # Note: Files and directories in lab environment may be different than the video demo
 
-# File System CREATE FILES FROM HERE
-# So let's say you are looking for files a user stored on a network drive, yet they don't know where or what they are named; just the type of file. That's not a problem with powerShell.
-    Help get-childitem
-    Get-ChildItem -Path c:\windows\web -Recurse
-    Get-ChildItem -Path c:\windows\web -Recurse | gm
-    Get-ChildItem -Path c:\windows\web -Recurse | where Extension -EQ '.PNG'
-    Get-ChildItem -Path c:\windows\web -Recurse | where Extension -EQ '.PNG' | ft Directory,Name,LastWriteTime
-#Now lets say I want to move files from 
-    Gcm *copy*
-    help Copy-Item -Examples
-    copy-item c:\windows\web -Destination c:\CopiedFolder -Recurse -Verbose
-    dir c:\CopiedFolder -recurse
-    move-item c:\CopiedFolder -Destination c:\MovedFolder -verbose
-    dir c:\MovedFolder -Recurse
-    Rename-Item c:\MovedFolder -NewName c:\RenamedFolder
-    dir c:\
+    # Searching the Windows File system for .PNG files
+        Help get-childitem
+        Get-ChildItem -Path c:\windows\web -Recurse
+        Get-ChildItem -Path c:\windows\web -Recurse | gm
+        Get-ChildItem -Path c:\windows\web -Recurse | where Extension -EQ '.PNG'
+        Get-ChildItem -Path c:\windows\web -Recurse | where Extension -EQ '.PNG' | ft Directory,Name,LastWriteTime
 
+    # Copying Files
+        Gcm *copy*
+        help Copy-Item -Examples
+        copy-item c:\windows\web -Destination c:\CopiedFolder -Recurse -Verbose
+        dir c:\CopiedFolder -recurse
+
+    # Moving files  
+        move-item c:\CopiedFolder -Destination c:\MovedFolder -verbose
+        dir c:\MovedFolder -Recurse
+        Rename-Item c:\MovedFolder -NewName c:\RenamedFolder
+        dir c:\

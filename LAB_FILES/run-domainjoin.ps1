db2d578ac96cfbb2b48223c0664a7471734b2771
mbender <mbender@bentech.net>
2021-07-13 11:53:21 -0500

    updates PS getting started

--- LAB_FILES/run-domainjoin.ps1
+++ LAB_FILES/run-domainjoin.ps1
@@ -9,6 +9,18 @@ $pwd = get-content c:/users/administrator/desktop/lab_files/companypw.txt
 $secpwd = ConvertTo-SecureString $pwd -AsPlainText -Force
 $admin = "company\administrator"
 $Cred = New-Object System.Management.Automation.PSCredential ($admin, $secpwd)
+$dc = "DC01"
+Write-Host "This script will rename the computer to $compname, and add it into domain $domain"
+Write-host ""
+Write-host "Because the domain controller $dc is building, this process may take a few minutes to complete"
+Write-host ""
+Write-host "After the script completes and the computer reboots, you can begin the lab"
+Write-host ""
+do {
+    Write-Host "Checking netlogon service on $DC"
+    $netlogonsvc =(Get-CimInstance -ClassName Win32_service -Filter "Name = 'netlogon'" -ComputerName $dc -ErrorAction SilentlyContinue).state 
+    sleep 15
+} until ($netlogonsvc -ne 'running')
 Write-host "Renaming computer to $compname"
 Rename-Computer -NewName $compname 
 Write-host "Adding computer to $domain domain"
